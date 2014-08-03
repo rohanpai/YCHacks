92454ca156ba92bd791c3cf3d9f49a86b1096727
Rohan Pai <me@rohanpai.com>
2014-08-02 18:59:47 -0700

    organizing code

--- test.py
+++ test.py
@@ -4,6 +4,8 @@ import time
 import math
 import os
 import uuid
+from PIL import Image
+from PIL import ImageChops
 
 SCREENSHOT_DIRECTORY = "screenshots"
 
@@ -20,15 +22,26 @@ class UITest:
     os.makedirs(os.path.join(SCREENSHOT_DIRECTORY, self.folder_uuid));
     
     browser = webdriver.Chrome()
+    #take a base screenshot first
     browser.get('http://www.google.com/')
-    browser.save_screenshot(SCREENSHOT_DIRECTORY+'/'+self.folder_uuid+'/'+str(int(time.time()))+'.png')
+    browser.save_screenshot(SCREENSHOT_DIRECTORY+'/'+self.folder_uuid+'/base.png')
+
+    #start making modificatons
+    #browser.save_screenshot(SCREENSHOT_DIRECTORY+'/'+self.folder_uuid+'/'+str(int(time.time()))+'.png')
     browser.quit()
 
-
+  def find_outlier(self):
+    print 'find outlier'
+    self.nearest_k = []
+    base_img = Image.open(SCREENSHOT_DIRECTORY+'/'+self.folder_uuid+'/base.png')
+    for f in os.listdir(os.path.join(SCREENSHOT_DIRECTORY, self.folder_uuid)):
+      if f.endswith(".png") and f != 'base.png':
+        print f
+      
 def main():
   my_test = UITest()
   my_test.run_ui_test()
-
+  my_test.find_outlier()
 
 if __name__ == '__main__':
   main()
