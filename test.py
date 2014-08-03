1cdc77638d6338f9d615739a1e81a295b3174286
Rohan Pai <me@rohanpai.com>
2014-08-02 17:54:51 -0700

    organizes screenshts into folders

--- test.py
+++ test.py
@@ -1,11 +1,31 @@
 #!/usr/bin/env python
 from selenium import webdriver
+import time
+import math
+import os
+import uuid
 
-selectors = ['#name', '#title']
+SCREENSHOT_DIRECTORY = "screenshots"
 
-browser = webdriver.Chrome()
-browser.get('http://www.google.com/')
-browser.save_screenshot('screenie.png')
-browser.quit()
+def run_ui_test():
+  selectors = ['#name', '#title']
+  folder_uuid= str(uuid.uuid4())
 
 
+  if not os.path.exists(SCREENSHOT_DIRECTORY):
+    os.makedirs(SCREENSHOT_DIRECTORY)
+  
+  os.makedirs(os.path.join(SCREENSHOT_DIRECTORY, folder_uuid));
+
+  browser = webdriver.Chrome()
+  browser.get('http://www.google.com/')
+  browser.save_screenshot(SCREENSHOT_DIRECTORY+'/'+folder_uuid+'/'+str(int(time.time()))+'.png')
+  browser.quit()
+
+
+def main():
+  run_ui_test()
+
+
+if __name__ == '__main__':
+  main()
